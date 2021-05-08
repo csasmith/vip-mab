5ba0c3cd30dbdcb2fd2e867476430399c6cda572
Christopher Smith <christopher.s.smith@stonybrook.edu>
2021-05-08 12:51:33 -0400

    Add comments, made num_malicious=0 work

--- malicious_bernoulli.py
+++ malicious_bernoulli.py
@@ -46,7 +46,7 @@ T = 250
 #random.seed(42)
 
 a = A.toarray() # make adjacency matrix an array for ease of use
-neighbors = [] # list of all agents' neighbors
+neighbors = [] # list of all agents' neighbors (wonder if we could do this with adjacency list)
 for i in range(len(a)):
     curr_neighbors = [] # neighbors of current agent
     for j in range(len(a)):
@@ -67,15 +67,16 @@ agent3_regret = []
 agent4_regret = []
 agent5_regret = []
 
-malicious = 0
+malicious = 0 # index of malicious agent
 num_malicious = 1
 
 for epoch in range(10):
     # initialize all vectors in a matrix for each time step, names corresponding to paper
-    n = [np.zeros((N,M)) for t in range(T+1)]
-    x = [np.zeros((N,M)) for t in range(T+1)]
-    X = [np.zeros((N,M)) for t in range(T+1)]
-    z = [np.zeros((N,M)) for t in range(T+1)]
+    # (wonder if we could do something like np.zeros((N,M,T))?)
+    n = [np.zeros((N,M)) for t in range(T+1)] # (num times agent i picked arm k until time t)
+    x = [np.zeros((N,M)) for t in range(T+1)] # (avg rwd of arm k at agent i until time t)
+    X = [np.zeros((N,M)) for t in range(T+1)] # (rwd of agent i at arm k at time t? I think it should be cumulative rwd of agent i at arm k until time t)
+    z = [np.zeros((N,M)) for t in range(T+1)] # (some kind of avg reward among all agents)
 
     # create rwds array to hold all rewards each agent picks
     rwds = [np.zeros(N) for t in range(T+1)]
@@ -96,37 +97,41 @@ for epoch in range(10):
 
     for t in range(1,T): # loop over time
         for agent in range(N): # loop through all agents
-            if agent == malicious:
+            if agent == malicious and num_malicious:
                 #X[t+1][agent][candidate] = 0.33 # this and n array shouldn't matter for malicious if always force z=0.33?
                 for arm in range(M):
                     rwds[t+1][agent] = 0.33 
                     z[t+1][agent][arm] = 0.33 # always force malicious t0 estimate 0.33 for every arm every time
             else:
                 
+                # (DECISION MAKING)
+
                 Q = [] # corresponds to Q in paper
         
                 for arm in range(M):
                     q = z[t][agent][arm] + np.sqrt((2*np.log(t))/(num_neighbors[agent]*n[t][agent][arm]))
                     Q.append(q)
             
-                if agent == malicious:
+                if agent == malicious and num_malicious: # so this should never run right? Because if agent == malicious is already up there...
                     candidate = np.argmin(arm_means) # set malicious agents' policy
                 else:
                     candidate = np.argmax(Q)
-        
+
                 X[t+1][agent][candidate] = np.random.binomial(size=1, n=1, p=arm_means[candidate]) # calculate reward for current agent's arm
                 rwds[t+1][agent] = X[t+1][agent][candidate] # keep track of chosen reward
+
+                # (UPDATING)
 
                 for arm in range(M): # update all arm estimations for this agent
                     if arm == candidate: # if chosen arm
                         n[t+1][agent][arm] = n[t][agent][arm] + 1
                         xsum = 0
-                        for time in range(t+1): # sum up all rewards so far
+                        for time in range(t+1): # sum up all rewards so far (Consider making X running sum?)
                             xsum += X[time][agent][arm]
                         x[t+1][agent][arm] = (1/n[t+1][agent][arm])*xsum
                     else: # if not chosen arm
                         n[t+1][agent][arm] = n[t][agent][arm]
-                        x[t+1][agent][arm] = x[t][agent][arm] # not mentioned in paper but seems necessary
+                        x[t+1][agent][arm] = x[t][agent][arm] # not mentioned in paper but seems necessary (I agree)
                 
                     #if num_neighbors[agent] - 1 > 2*num_malicious:
                     zsum = 0
@@ -137,6 +142,7 @@ for epoch in range(10):
                             zvals.append(z[t][neighbor][arm])
                     zvals.sort()
                     
+                    # (filter out potentially malicious high values)
                     new_zvals = [] 
                     counter = 0
                     for zval in zvals:
@@ -144,7 +150,8 @@ for epoch in range(10):
                             counter += 1
                         else:
                             new_zvals.append(zval)
-                
+
+                    # (filter out potentially malicious low values)
                     new_new_zvals = [] 
                     counter = 0
                     for zval in reversed(new_zvals):
@@ -156,6 +163,7 @@ for epoch in range(10):
                     new_new_zvals.append(z_agent)
                     
                     for zval in new_new_zvals:
+                        # (PAPER AMBIGUOUS: is xbar(t+1) - xbar(t) part of the summation???)
                         zsum += zval + x[t+1][agent][arm] - x[t][agent][arm] # calculate sum for z update
                     z[t+1][agent][arm] = (1/(len(new_new_zvals)))*zsum # update current agent's z
 
